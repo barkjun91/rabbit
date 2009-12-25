3e50497288ef42a6583537514372e189a4ebc2a1
goyange <goyange@bakyoungjunyi-macbook.local>
2009-12-25 23:08:24 +0900

    Person(enemy) 클래스 추가 및 Load_Peroson 구현 / 오류있음 미투밴드 참고

--- tutorial.py
+++ tutorial.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 
 SCREEN_SIZE = (800, 600) # screen size set
 
+PERSON_LIST = []
 
 #load image. where? = main/data/tutorial/~
 def load_image(name, colorkey=None):
@@ -22,6 +23,23 @@ def load_image(name, colorkey=None):
         image.set_colorkey(colorkey, RLEACCEL)
     return image
 
+def load_person(data):
+    person = open('data/tutorial/'+data)
+    p_count = int(person.readline())
+    try:
+	for i in range(0, p_count, 1): #c -> for(i = 0; i<1; i ++)
+	    print person.readline()
+	    x = person.readline()
+	    y = person.readline()
+	    hp = person.readline()
+	    PERSON_LIST.insert(i, Person("m_farmer.bmp", 2, (x,y),hp))
+    except pygame.error, message:
+	print 'Cannot load Person Data'
+	raise SystemExit, message
+
+    return p_count
+
+
 class Object:
     def __init__(self, image, speed, (x, y)):
 	self.image = load_image(image).convert_alpha()
@@ -29,6 +47,11 @@ class Object:
 	self.pos_x, self.pos_y = (x, y)
     def draw(self, screen):
 	screen.blit(self.image, (self.pos_x, self.pos_y))
+
+class Person(Object):
+    def __init__(self, image, speed, (x,y), hp):
+	Object.__init__(self, image, speed, (x,y))
+	self.hp = hp
 
 class Player(Object):
     def __init__(self, image, speed, (x, y), weapon, clothes, rabbits):
@@ -44,6 +67,9 @@ class Player(Object):
 
 
 
+
+
+
 class Weapon:
     def __init__(self, weapon):
 	self.name = weapon
@@ -54,6 +80,9 @@ def tutorial_main(screen):
     player = Player("player.bmp", 2, (220,490), "test", "test", 0)
     maps = map.Map("map.txt", "tiles.png")
     camera = map.Camera(screen)
+    count = load_person("person.txt")
+
+    print PERSON_LIST[0].hp
 
     while 1:
 	screen.fill((255,255,255))
