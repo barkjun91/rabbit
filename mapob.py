5fca7f9bb300c09275c31f43db2b1516d2d403c0
goyange <goyange@bakyoungjunyi-macbook.local>
2010-01-04 02:41:47 +0900

    간혹 나무의 반투명화가 풀리지 않던 현상 수정

--- mapob.py
+++ mapob.py
@@ -12,7 +12,7 @@ class Mapob(pygame.sprite.Sprite):
 	self.object = map.load_image(name+".png", -1)
 	self.pos_x = x
 	self.pos_y = y
-	self.area = map.load_image("area_"+name+".png")
+	self.area = map.load_image("area_"+name+".png", -1)
 	self.s_x, self.s_y = shadow(name, self.pos_x, self.pos_y)
 	self.rect = Rect((self.s_x, self.s_y, 
                           self.area.get_width(), self.area.get_height()))
@@ -23,9 +23,12 @@ class Mapob(pygame.sprite.Sprite):
 	screen.blit(self.area, (self.s_x-camera.px, self.s_y))
 	x1 = self.pos_x+self.object.get_width()
 	y1 = self.pos_y+self.object.get_height()
-	if self.pos_x <= player.pos_x+camera.px <= x1:	
-            if y1 >= player.pos_y+player.image.get_height() >= self.pos_y:
-	        self.object.set_alpha(125)
+	print y1
+	print self.pos_y
+	print player.pos_y+player.image.get_height() 
+	print "----"
+	if self.pos_x <= player.pos_x+camera.px <= x1 and y1 >= player.pos_y+player.image.get_height() >= self.pos_y:	
+	    self.object.set_alpha(125)
 	else:
 	    self.object.set_alpha(255)
         screen.blit(self.object, (self.pos_x-camera.px, self.pos_y))
